af052c9646cb55160afa6295a49f0e7deeadae0c
Nataliia Podoroha <ingenuous999@gmail.com>
2024-10-28 10:53:01 +0100

    remove token auth

--- notification_system/settings.py
+++ notification_system/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    'rest_framework.authtoken',
     "notifications",
 ]
 
@@ -119,15 +118,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        # 'rest_framework.authentication.TokenAuthentication',
-    ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        # 'rest_framework.permissions.IsAuthenticated',
-    ],
-}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
